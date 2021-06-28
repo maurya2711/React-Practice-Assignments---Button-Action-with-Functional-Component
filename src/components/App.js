df978ff36869d85ace21fccfd7a27582e1aa5f06
Sharad Maurya <sharadmaurya4600@gmail.com>
2021-06-28 13:29:35 +0530

    First Build.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { useState } from "react";
+import "../styles/App.css";
 
 function App() {
+  const [para, showPara] = useState(false);
+
+  const changeClassName = () => {
+    showPara(!para);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={(e) => changeClassName(e)}>
+        Click Me
+      </button>
+
+      <p style={{ display: para ? "block" : "none" }}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/Para.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Para() {
+  return (
+    <p id="para">
+      Hello, I've learnt to use the full-stack evaluation tool. This makes me so
+      happy
+    </p>
+  );
+}
+
+export default Para;
